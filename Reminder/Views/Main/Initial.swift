81f3e3534dcd0a2c9523ac57af2032646fc53e88
hk14004 <hardijs.kirsis@gmail.com>
2020-07-30 20:24:50 +0300

    Add tiles; Extract subviews to make code cleaner

--- Reminder/Views/Main/Initial.swift
+++ Reminder/Views/Main/Initial.swift
@@ -13,13 +13,15 @@ struct Initial: View {
     @State var isSearching: Bool = false
     var body: some View {
         VStack() {
+            
             if !isSearching {
-            HStack() {
-                Spacer()
-                EditButton().padding([.top, .trailing]).padding(.bottom, -15)
-                }.background(Color(hex: "F2F2F7").edgesIgnoringSafeArea(.top))
+                ToolBar()
             }
+            
             SearchBar(searchText: $searchText, isEditing: $isSearching)
+            
+            Tiles()
+            
             Spacer()
             Text(searchText)
             Spacer()
@@ -30,5 +32,86 @@ struct Initial: View {
 struct Initial_Previews: PreviewProvider {
     static var previews: some View {
         Initial()
+    }
+}
+
+struct ToolBar: View {
+    var body: some View {
+        HStack() {
+            Spacer()
+            EditButton().padding([.top, .trailing]).padding(.bottom, -15)
+        }.background(Color(hex: "F2F2F7").edgesIgnoringSafeArea(.top))
+    }
+}
+
+struct Tiles: View {
+    var body: some View {
+        VStack() {
+            HStack() {
+                TodayTile()
+                ScheduledTile()
+            }.animation(.default)
+            
+            AllTile()
+        }
+    }
+}
+
+struct TodayTile: View {
+    var body: some View {
+        VStack() {
+            HStack() {
+                Image(systemName: "calendar.circle.fill").resizable().frame(width:35, height:35).foregroundColor(.blue)
+                
+                Spacer()
+                Text("0").font(.largeTitle).bold()
+            }.padding(5).padding(.bottom, -10).padding(.leading, 5).background(Color.white)
+            
+            HStack {
+                Text("Today").foregroundColor(Color(hex: "87878C")).bold().padding(.leading, 5)
+                Spacer()
+            }.padding(5).padding(.top, -5).background(Color.white)
+            
+        }.background(Color.white).cornerRadius(10).padding(.leading, 10).padding(.trailing, 5)
+    }
+}
+
+struct ScheduledTile: View {
+    var body: some View {
+        VStack() {
+            HStack() {
+                
+                Image(systemName: "clock").resizable().frame(width:25, height:25).foregroundColor(.orange).overlay(RoundedRectangle(cornerRadius: 15)
+                    .stroke(Color.orange, lineWidth: 7))
+                Spacer()
+                Text("0").font(.largeTitle).bold()
+            }.padding(5).padding(.bottom, -10).padding(.leading, 7).background(Color.white)
+            HStack {
+                Text("Scheduled").foregroundColor(Color(hex: "87878C")).bold().padding(.leading, 5)
+                Spacer()
+            }.padding(5).padding(.top, -5).background(Color.white)
+            
+        }.background(Color.white).cornerRadius(10).padding(.trailing, 10).padding(.leading, 5)
+    }
+}
+
+struct AllTile: View {
+    var body: some View {
+        HStack() {
+            VStack() {
+                HStack() {
+                    Image(systemName: "tray.fill").resizable().aspectRatio(contentMode: .fit).frame(width:30, height:35).foregroundColor(.gray)
+                    
+                    Spacer()
+                    Text("0").font(.largeTitle).bold()
+                }.padding(5).padding(.bottom, -10).padding(.leading, 5).background(Color.white)
+                
+                HStack {
+                    Text("All").foregroundColor(Color(hex: "87878C")).bold().padding(.leading, 5)
+                    Spacer()
+                }.padding(5).padding(.top, -5).background(Color.white)
+                
+            }.background(Color.white).cornerRadius(10).padding(.leading, 10).padding(.trailing, 5)
+        }.animation(.default)
     }
 }
