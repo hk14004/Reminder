4e86e26354ad9c15ef4dfe337aae0dca8b4e004f
hk14004 <hardijs.kirsis@gmail.com>
2020-08-09 02:28:43 +0300

    Extract AddReminder view subviews to functions

--- Reminder/Views/AddReminder/AddReminderView.swift
+++ Reminder/Views/AddReminder/AddReminderView.swift
@@ -21,70 +21,58 @@ struct AddReminderView: View {
     @State var newReminderText: String = ""
     @State var addingNewReminder: Bool = false
     @State var completeReminder: Bool = false
-    
+        
     var body: some View {
         VStack() {
-            HStack() {
-                Text("\(reminderList.name ?? "")").foregroundColor(COLORS[Int(reminderList.iconColor)]).font(.title).bold()
-                Spacer()
-            }.padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+            reminderListNameView()
             if reminderArray.count == 0 && !addingNewReminder {
-                VStack {
-                    Color.black.opacity(0.001)
-                    Spacer()
-                }.onTapGesture {
-                    self.toggleAddReminder()
-                }
+                emptyAddReminderToggleView()
                 Text("No Reminders").font(.title).foregroundColor(.gray).onTapGesture {
                     self.toggleAddReminder()
                 }
             } else {
-                //So that we can just relead new table without animation on insert
+                //So that we can just reload new table without animation on insert
                 if addingNewReminder {
-                    ReminderListContainerView(reminderArray: reminderArray, reminderList: $reminderList, newReminderText: $newReminderText, addingNewReminder: $addingNewReminder, completeReminder: $completeReminder)
+                    reminderListContainerView()
                 } else {
-                    ReminderListContainerView(reminderArray: reminderArray, reminderList: $reminderList, newReminderText: $newReminderText, addingNewReminder: $addingNewReminder, completeReminder: $completeReminder)
+                    reminderListContainerView()
                 }
             }
-            VStack {
-                Color.black.opacity(0.001)
-                Spacer()
-            }.onTapGesture {
-                self.toggleAddReminder()
-            }
-            
-            HStack() {
-                Button(action: {
-                    self.toggleAddReminder()
-                }) {
-                    Image(systemName: "plus.circle.fill").resizable().aspectRatio(contentMode: .fit).frame(width: 25, height: 25).foregroundColor(COLORS[Int(reminderList.iconColor)])
-                    Text("New Reminder").foregroundColor(COLORS[Int(reminderList.iconColor)]).bold()
-                }
-                Spacer()
-            }.padding(15)
+            emptyAddReminderToggleView()
+            addNewReminderButtonView()
         }.background(Color("customForeground")).navigationBarItems(trailing:
-            
             Button(action: {}) {
                 Image(systemName: "ellipsis").resizable().aspectRatio(contentMode: .fit).frame(width: 22, height: 22).foregroundColor(Color.blue).padding(5).background(Color("CustomBackground")).cornerRadius(30)
             }
         ).navigationBarColor(.white).navigationBarTitle("", displayMode: .inline)
     }
     
-    func toggleAddReminder() {
+    private func reminderListContainerView() -> some View {
+        List() {
+            ForEach(self.reminderArray, id: \.self) { currentReminder in
+                ReminderCellView(reminder: currentReminder, reminderListColor: COLORS[Int(self.reminderList.iconColor)])
+            }.onDelete(perform: self.deleteReminder)
+            if addingNewReminder {
+                NewReminderCellView(reminderText: $newReminderText, completed: $completeReminder, reminderColor: COLORS[Int(reminderList.iconColor)], addingNewReminder: $addingNewReminder, reminderList: reminderList)
+            }
+        }.frame(height: CGFloat( self.reminderArray.count * 50 + (addingNewReminder ? 42 : 0))).onAppear {
+            UITableView.appearance().separatorInset = UIEdgeInsets(top: 0, left: 60, bottom: 0, right: 0)
+        }
+    }
+    
+    private func toggleAddReminder() {
         if !self.newReminderText.isEmpty && addingNewReminder {
-            // save
-            print("SAVE!")
             let r = ReminderEntity(context: managedObjectContext)
             r.completed = self.completeReminder
-            r.orderPriority = getLastID() + 1
+            r.orderPriority = getLastReminderID() + 1
             r.reminderList = self.reminderList.id?.uuidString
             r.text = self.newReminderText
             r.id = UUID()
             
-            save()
+            (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
         }
         
-        reminderArray.forEach{
+        reminderArray.forEach {
             if $0.text == "" {
                 self.managedObjectContext.delete($0)
             }
@@ -92,7 +80,6 @@ struct AddReminderView: View {
         
         self.newReminderText = ""
         self.completeReminder = false
-        
         self.addingNewReminder = self.addingNewReminder ? false : true
         
         if addingNewReminder {
@@ -100,7 +87,7 @@ struct AddReminderView: View {
         }
     }
     
-    private func getLastID() -> Int64 {
+    private func getLastReminderID() -> Int64 {
         let fetchRequest = NSFetchRequest<ReminderEntity>(entityName: "ReminderEntity")
         
         fetchRequest.fetchLimit = 1
@@ -117,60 +104,39 @@ struct AddReminderView: View {
         }
     }
     
-    func save() {
-        do {
-            try self.managedObjectContext.save()
-        } catch {
-            print("Save failed \(error.localizedDescription)")
-            // TODO: Display alert
-        }
-    }
-
-    func delete(at offsets: IndexSet) {
+    private func deleteReminder(at offsets: IndexSet) {
         for index in offsets {
             let reminder = reminderArray[index]
             managedObjectContext.delete(reminder)
         }
-        save()
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
-}
-
-struct ReminderListContainerView: View {
-    @Environment(\.managedObjectContext) var managedObjectContext
-    var reminderArray: FetchedResults<ReminderEntity>
+    private func emptyAddReminderToggleView() -> some View {
+        VStack {
+            Color.black.opacity(0.001)
+            Spacer()
+        }.onTapGesture {
+            self.toggleAddReminder()
+        }
+    }
     
-    @Binding var reminderList: ReminderListEntity
-    @Binding var newReminderText: String
-    @Binding var addingNewReminder: Bool
-    @Binding var completeReminder: Bool
-    var body: some View {
-        List() {
-            ForEach(self.reminderArray, id: \.self) { currentReminder in
-                ReminderCellView(reminder: currentReminder, reminderListColor: COLORS[Int(self.reminderList.iconColor)])
-            }.onDelete(perform: self.delete)
-            if addingNewReminder {
-                NewReminderCellView(reminderText: $newReminderText, completed: $completeReminder, reminderColor: COLORS[Int(reminderList.iconColor)], addingNewReminder: $addingNewReminder, reminderList: reminderList)
+    private func reminderListNameView() -> some View {
+        HStack() {
+            Text("\(reminderList.name ?? "")").foregroundColor(COLORS[Int(reminderList.iconColor)]).font(.title).bold()
+            Spacer()
+        }.padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
+    }
+    
+    private func addNewReminderButtonView() -> some View {
+        HStack() {
+            Button(action: {
+                self.toggleAddReminder()
+            }) {
+                Image(systemName: "plus.circle.fill").resizable().aspectRatio(contentMode: .fit).frame(width: 25, height: 25).foregroundColor(COLORS[Int(reminderList.iconColor)])
+                Text("New Reminder").foregroundColor(COLORS[Int(reminderList.iconColor)]).bold()
             }
-        }.frame(height: CGFloat( self.reminderArray.count * 50 + (addingNewReminder ? 42 : 0))).onAppear {
-            UITableView.appearance().separatorInset = UIEdgeInsets(top: 0, left: 60, bottom: 0, right: 0)
-        }
-    }
-    
-    func save() {
-        do {
-            try self.managedObjectContext.save()
-        } catch {
-            print("Save failed \(error.localizedDescription)")
-            // TODO: Display alert
-        }
-    }
-
-    func delete(at offsets: IndexSet) {
-        for index in offsets {
-            let reminder = reminderArray[index]
-            managedObjectContext.delete(reminder)
-        }
-        save()
+            Spacer()
+        }.padding(15)
     }
 }
